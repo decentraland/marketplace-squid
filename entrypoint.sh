4e6554187a7fc839b32aef601e89cf2962878006
Juanma Hidalgo <juanma06@gmail.com>
2024-06-19 19:31:51 +0200

    feat: set a new user per deployment as well

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,19 @@
-# Generate a unique schema name using a timestamp
-SCHEMA_NAME="schema_$(date +%s)"
+#!/bin/sh
 
-# Connect to the database and create the new schema, and set it for squid-writer
+# Generate a unique schema name and user credentials using a timestamp
+SCHEMA_NAME="schema_$(date +%s)"
+NEW_DB_USER="user_$(date +%s)"
+
+# Connect to the database and create the new schema and user
 psql -v ON_ERROR_STOP=1 --username "$DB_USER" --dbname "$DB_PASSWORD" <<-EOSQL
   CREATE SCHEMA $SCHEMA_NAME;
-  ALTER USER squid-writer SET search_path TO $SCHEMA_NAME;
+  CREATE USER $NEW_DB_USER WITH PASSWORD '$DB_PASSWORD';
+  GRANT ALL PRIVILEGES ON SCHEMA $SCHEMA_NAME TO $NEW_DB_USER;
+  ALTER USER $NEW_DB_USER SET search_path TO $SCHEMA_NAME;
 EOSQL
 
-# Construct the DB_URL with the squid-writer user
-export DB_URL=postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
+# Construct the DB_URL with the new user
+export DB_URL=postgresql://$NEW_DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME
 
 # Build the squid
 sqd build
