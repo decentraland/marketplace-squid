cbecdda8835b52c39e473eef6149808fd66765a9
Juanma Hidalgo <juanma06@gmail.com>
2024-08-12 09:30:43 +0200

    fix: turn all addresses lowercase for Polygon

--- src/polygon/addresses/mainnet.ts
+++ src/polygon/addresses/mainnet.ts
@@ -1,13 +1,13 @@
-export const CollectionFactory = "0xB549B2442b2BD0a53795BC5cDcBFE0cAF7ACA9f8";
-export const CollectionFactoryV3 = "0x3195e88aE10704b359764CB38e429D24f1c2f781";
+export const CollectionFactory = "0xb549b2442b2bd0a53795bc5cdcbfe0caf7aca9f8";
+export const CollectionFactoryV3 = "0x3195e88ae10704b359764cb38e429d24f1c2f781";
 
 export const Bid = "0x0000000000000000000000000000000000000000";
 export const BidV2 = "0xb96697fa4a3361ba35b774a42c58daccaad1b8e1";
 
 export const Marketplace = "0x02080031b45a3c67d338dd4a2cc309d28756a160";
-export const MarketplaceV2 = "0x480a0f4e360E8964e68858Dd231c2922f1df45Ef";
+export const MarketplaceV2 = "0x480a0f4e360e8964e68858dd231c2922f1df45ef";
 
-export const OldCommittee = "0x71d9350Ef44E1e451F00e447C0DfF2d1FB75510a";
+export const OldCommittee = "0x71d9350ef44e1e451f00e447c0dff2d1fb75510a";
 export const Committee = "0xaeec95a8aa671a6d3fec56594827d7804964fa70";
 
 export const CollectionStore = "0x214ffc0f0103735728dc66b61a22e4f163e275ae";
